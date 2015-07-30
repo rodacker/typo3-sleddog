35097c6a0cc3650d569cff6b539b4a883d4f1ace
Patrick Rodacker <patrick.rodacker@the-reflection.de>
2015-07-30 19:11:21 +0200

    added ext_emconf.php file

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "sleddog".
+ *
+ * Auto generated 30-07-2015 13:03
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Kiruna Sleddog Tours',
+	'description' => 'Extension for the project Kiruna Sleddog Tours',
+	'category' => 'fe',
+	'version' => '1.0.0',
+	'state' => 'beta',
+	'uploadfolder' => true,
+	'createDirs' => NULL,
+	'clearcacheonload' => true,
+	'author' => 'Patrick Rodacker',
+	'author_email' => 'patrick.rodacker@gmail.com',
+	'author_company' => '',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '6.2.10-7.99.99',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+);
+
